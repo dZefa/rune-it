9b0366dcc399b320f5211cdbf7e4bc9cedd134a2
Daniel Chong <danchong625@gmail.com>
2017-12-22 19:11:06 -0800

    [feat] initializes basic node express server

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const Morgan = require('morgan');
+const Dotenv = require('dotenv');
+
+// Initialize environment variables
+Dotenv.config({ path: path.resolve(__dirname, 'server.env')});
+
+// Initialize ExpressJS & Port
+const app = express();
+const PORT = process.env.PORT;
+
+// Initialize Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(Morgan());
+
+// Serve static files
+app.use('/', express.static(path.resolve(__dirname, '../client/dist')));
+app.use('*', (req, res) => {
+  fs.readFile(path.resolve(__dirname, '../client/dist/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});
