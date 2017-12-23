b15caa79d98566e08d154e8eef651e6b9cd63a28
Daniel Chong <danchong625@gmail.com>
2017-12-22 19:35:23 -0800

    [feat] initializes react client

---
+++ client/src/app.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+
+  }
+
+  render() {
+    return (
+      <div>
+        This is APP view
+      </div>
+    )
+  }
+};
+
+export default App;
