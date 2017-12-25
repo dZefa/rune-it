f8ec50c41eb89a6dde26b97151c16f6ae8f09b32
Daniel Chong <danchong625@gmail.com>
2017-12-24 21:31:19 -0800

    [ops] initializes mongodb for matchlist

---
+++ services/matchList-service/db/index.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const log = require('../lib/log');
+
+const uri = `${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}`;
+
+mongoose.connect(uri);
+
+mongoose.connection.on('connected', () => {
+  log(`Mongoose default connection open to ${uri}`);
+});
+
+mongoose.connection.on('error', (err) => {
+  log(`Mongoose default connection error: ${err}`);
+});
+
+mongoose.connection.on('disconnected', () => {
+  log(`Mongoose default connection disconnected`);
+});
+
+const db = mongoose.connection;
+
+module.exports = db;
