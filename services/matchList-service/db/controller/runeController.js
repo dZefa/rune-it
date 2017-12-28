03fdd7f46da27f804a5ba4161f35e10b034bc6a8
Daniel Chong <danchong625@gmail.com>
2017-12-28 15:31:02 -0800

    [ops] adds add match per summoner method

--- services/matchList-service/db/controller/runeController.js
+++ services/matchList-service/db/controller/runeController.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 const { MatchList } = require('../models/matchList');
 const { Rune } = require('../models/rune');
 const log = require('../../lib/log');
+const { createRuneHash } = require('../../lib/createRuneHash');
 
 // Promisify mongoose queries
 mongoose.Promise = bluebird;
@@ -22,11 +23,55 @@ const checkMatchList = async (matchIds) => {
         log(`New match added. Matchid: ${key}`);
       }
     }
-    
+
+    matchList.forEach(match => {
+      addMatchPerSummoner(match);
+    });
+    log(`Finished adding new runes and matches`);
   } catch (err) {
     log(`Error in checkList: ${err}`);
     return err;
   }
+};
+
+const addMatchPerSummoner = async (matchId) => {
+  axios.get({
+    url: `${process.env.RIOT_URL}/match/v3/matches/${matchId}`,
+    method: 'get',
+    headers: {
+      'X-Riot-Token': `${process.env.RIOT_TOKEN}`
+    }
+  })
+    .then(response => {
+      const accountIds = {};
+      response.participantIdentities.forEach(player => {
+        let participantId = player.participantId;
+        accountIds.participantId = player.player.accountId;
+      });
+
+      response.participants.forEach(player => {
+        let runes = {
+          primaryStyle: player.stats.perkPrimaryStyle,
+          subStyle: player.stats.perkSubStyle,
+          perk0: player.stats.perk0,
+          perk1: player.stats.perk1,
+          perk2: player.stats.perk2,
+          perk3: player.stats.perk3,
+          perk4: player.stats.perk4,
+          perk5: player.stats.perk5,
+        };
+        const runeHash = await createRuneHash(runes);
+        const newRune = new Rune({
+          matchId: `${matchId}`,
+          championId: `${player.championId}`,
+          accountId: `${accountIds[player.participantId]}`,
+          runeHash: `${runeHash}`,
+          matchResult: player.stats.win
+        });
+        await newRune.save();
+        log(`New Rune added`);
+      });
+    })
 };
 
 module.exports = { checkMatchList };
