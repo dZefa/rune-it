52a1e5d340e178acfe81ba995e39ffc2f5d27da5
Daniel Chong <danchong625@gmail.com>
2017-12-28 15:46:31 -0800

    [ops] initializes rune hasher

---
+++ services/matchList-service/lib/createRuneHash.js
@@ -0,0 +1,27 @@
+const createRuneHash = (runes) => {
+  const runeHash = new Array(8).fill(0);
+
+  for (let key in runes) {
+    if (key === 'primaryStyle') {
+      runeHash[0] = runes[key];
+    } else if (key === 'subStyle') {
+      runeHash[1] = runes[key];
+    } else if (key === 'perk0') {
+      runeHash[2] = runes[key];
+    } else if (key === 'perk1') {
+      runeHash[3] = runes[key];
+    } else if (key === 'perk2') {
+      runeHash[4] = runes[key];
+    } else if (key === 'perk3') {
+      runeHash[5] = runes[key];
+    } else if (key === 'perk4') {
+      runeHash[6] = runes[key];
+    } else if (key === 'perk5') {
+      runeHash[7] = runes[key]
+    }
+  }
+
+  return runeHash.join();
+}
+
+module.exports = { createRuneHash };
