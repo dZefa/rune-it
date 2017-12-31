c75cced14355c4de78fedef63bedd3245a23006d
Daniel Chong <danchong625@gmail.com>
2017-12-30 17:33:01 -0800

    [feat] updates service with appropriate endpoints

--- services/matchList-service/index.js
+++ services/matchList-service/index.js
@@ -4,12 +4,13 @@ const fs = require('fs');
 const cors = require('cors');
 const Dotenv = require('dotenv');
 
+// Initialize environment variables
+Dotenv.config({ path: path.resolve(__dirname, './matchList.env')});
+
 const db = require('./db/index');
 const log = require('./lib/log');
-const { updateRunes } = require('./lib/playerList');
-
-// Initialize environment variables
-Dotenv.config({ path: path.resolve(__dirname, 'matchList.env')});
+const { updateMatch } = require('./lib/playerList');
+const { updateRunes } = require('./db/controller/runeController');
 
 // Initialize ExpressJS & Port
 const app = express();
@@ -19,6 +20,7 @@ const PORT = process.env.PORT;
 app.use(cors());
 
 // Initialize Service
+app.put('/updateMatch', updateMatch);
 app.post('/updateRunes', updateRunes);
 
 app.listen(PORT, () => {
