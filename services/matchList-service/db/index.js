e9e141ffcc5f3e4303d41e9fc3324f530d6aeb23
Daniel Chong <danchong625@gmail.com>
2017-12-30 17:35:00 -0800

    [feat] fixes env variable names

--- services/matchList-service/db/index.js
+++ services/matchList-service/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const log = require('../lib/log');
 
-const uri = `${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}`;
+const uri = `${process.env.MONGO_URL}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}`;
 
 mongoose.connect(uri);
 
