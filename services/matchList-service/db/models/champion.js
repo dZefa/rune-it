06dc8ca9374bfb557c622dbf4cda3946cd65768d
Daniel Chong <danchong625@gmail.com>
2017-12-30 17:33:51 -0800

    [ops] adds champion schema to service

---
+++ services/matchList-service/db/models/champion.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const championSchema = mongoose.Schema({
+  championId: {
+    type: String,
+    required: true,
+  },
+  championName: {
+    type: String,
+    required: true,
+  },
+  popularRune: {
+    type: String,
+    required: false,
+  },
+  popularWR: {
+    type: String,
+    required: false,
+  },
+  bestRune: {
+    type: String,
+    required: false,
+  },
+  bestWR: {
+    type: String,
+    required: false,
+  },
+  totalGames: {
+    type: String,
+    required: false,
+  },
+});
+
+const Champion = mongoose.model('Champion', championSchema);
+
+module.exports = { Champion }
