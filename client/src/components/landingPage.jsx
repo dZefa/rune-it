611c93a7eca6b0d299c7927e527de858b340f2f4
Daniel Chong <danchong625@gmail.com>
2017-12-31 22:02:06 -0800

    [feat] initializes landing page

---
+++ client/src/components/landingPage.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>hi</div>
+    )
+  }
+};
+
+export default Landing;
