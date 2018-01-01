fc52583d741e50c679d1637fcb34a62f933240d0
Daniel Chong <danchong625@gmail.com>
2017-12-31 22:01:42 -0800

    [feat] adds browser routing

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './app.jsx';
 
-render(<App />, document.getElementById('root'));
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  , 
+  document.getElementById('root')
+);
