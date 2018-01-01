59d7a5735b32cf3b50213c2dc218c5d566d6ee0a
Daniel Chong <danchong625@gmail.com>
2017-12-31 23:38:09 -0800

    [feat] adds in champions to state

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router';
+import axios from 'axios';
 
 import Landing from './components/landingPage.jsx';
 
@@ -8,15 +9,30 @@ class App extends Component {
     super();
 
     this.state = {
-
+      champions: [],
     }
   }
 
+  componentDidMount() {
+    // const thisState = this;
+    // axios.get(`${process.env.REST_SERVER_URL}:${process.env.REST_SERVER_PORT}/api/getChamps`)
+    //   .then(res => {
+    //     thisState.setState({
+    //       champions: res.data.result,
+    //     });
+    //   })
+    //   .catch(err => {
+    //     console.log(`Error grabbing champions from REST server. Error: ${err}`);
+    //   });
+  }
+
   render() {
+    const { champions } = this.state;
+
     return (
       <Switch>
         <Route exact path="/" component={() => (
-          <Landing />)} />
+          <Landing champions={champions} />)} />
       </Switch>
     )
   }
