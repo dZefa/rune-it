e86476552b5d92ab44564b8ff82286a407047a9d
Daniel Chong <danchong625@gmail.com>
2017-12-31 23:38:32 -0800

    [feat] adds dropdown to incorporate list of champions

--- client/src/components/landingPage.jsx
+++ client/src/components/landingPage.jsx
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 
 class Landing extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   render() {
+    const { champions } = this.props;
+
     return (
-      <div>hi</div>
+      <div>
+        <div className="row justify-content-md-center align-items-center" style={{ height: "100vh" }}>
+          <div className="col-8" style={{ textAlign: "center" }}>
+            <h1>Welcome to Rune IT</h1>
+            <div className="row">
+              <form>
+                <div className="form-group">
+                  <label htmlFor="champSelect">Select a Champion</label>
+                  <select className="form-control" id="champSelect">
+                    {
+                      champions.map(champ => (
+                        <option>${champ.name}</option>
+                      ))
+                    }
+                  </select>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 };
